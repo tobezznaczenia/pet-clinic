04c6ed0ca6e6496fcbace47cce10174f24cf66df
tobezznaczzenia <50057081o@gmail.com>
2020-10-15 21:37:38 +0200

    Add specialities bootstrap data

--- pet-clinic-data/src/main/java/com/example/petclinic/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/map/VetMapService.java
@@ -1,6 +1,7 @@
 package com.example.petclinic.map;
 
 import com.example.petclinic.model.Vet;
+import com.example.petclinic.services.SpecialityService;
 import com.example.petclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,13 @@ import java.util.Set;
 
 @Service
 public class VetMapService  extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetMapService(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Vet findById(Long id) {
         return super.findById(id);
@@ -15,6 +23,13 @@ public class VetMapService  extends AbstractMapService<Vet, Long> implements Vet
 
     @Override
     public Vet save(Vet object) {
+        if(object.getSpecialities().size() > 0){
+            object.getSpecialities().forEach(speciality -> {
+                if(speciality.getId()==null){
+                    speciality.setId(specialityService.save(speciality).getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
