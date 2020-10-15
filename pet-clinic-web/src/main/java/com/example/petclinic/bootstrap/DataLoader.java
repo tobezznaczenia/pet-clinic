49b0d6468a65f680e857aa46af606170bb46d1ee
tobezznaczzenia <50057081o@gmail.com>
2020-10-15 21:29:58 +0200

    Add specialities bootstrap data

--- pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package com.example.petclinic.bootstrap;
 
-import com.example.petclinic.model.Owner;
-import com.example.petclinic.model.Pet;
-import com.example.petclinic.model.PetType;
-import com.example.petclinic.model.Vet;
+import com.example.petclinic.model.*;
 import com.example.petclinic.services.OwnerService;
 import com.example.petclinic.services.PetTypeService;
+import com.example.petclinic.services.SpecialityService;
 import com.example.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,18 +17,26 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialityService specialityService;
 
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialityService = specialityService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        if(petTypeService.findAll().size()==0){
+            loadData();
+        }
+    }
+
+    private void loadData() {
         Owner owner1 = new Owner();
         owner1.setFirstName("Sansa");
         owner1.setLastName("Smith");
@@ -51,9 +57,14 @@ public class DataLoader implements CommandLineRunner {
         owner1.getPets().add(pet1);
         ownerService.save(owner1);
 
+        Speciality speciality1 = new Speciality();
+        speciality1.setDescription("radiology");
+        Speciality savedSpeciality = specialityService.save(speciality1);
+
         Vet vet1 = new Vet();
         vet1.setFirstName("Monique");
         vet1.setLastName("Morrone");
+        vet1.getSpecialities().add(savedSpeciality);
         vetService.save(vet1);
     }
 }
