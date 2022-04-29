c6c3fbc77b231739684f45424ae325f2f7459b2c
tobezznaczenia <32895536+tobezznaczenia@users.noreply.github.com>
2022-04-29 16:19:56 +0200

    Update PetClinicApplication.java

--- pet-clinic-web/src/main/java/com/example/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/PetClinicApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetClinicApplication {
 	private void metodka(){
-		Integer liczba = null;
-        	int liczba2 = liczba.intValue();
+		long zmienna = 100000L;
+        	int zmienna2 = 10 + (int) zmienna;
 		System.out.println("metodka");
 	}
 	public static void main(String[] args) {
