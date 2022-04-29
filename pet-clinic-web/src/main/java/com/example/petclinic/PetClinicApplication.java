5ec4620120a3636022c686408a984b57efe90e14
tobezznaczenia <32895536+tobezznaczenia@users.noreply.github.com>
2022-04-29 17:35:08 +0200

    Update PetClinicApplication.java

--- pet-clinic-web/src/main/java/com/example/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/PetClinicApplication.java
@@ -8,6 +8,11 @@ public class PetClinicApplication {
 	private void metodka(){
 		long zmienna = 100000L;
         	int zmienna2 = 10 + (int) zmienna;
+		int [] a = {1, 2, 3, 4};
+		int sum = 0;
+		for (int i = 0; i <= a.length; i++) { // BAD
+		    sum += a[i];
+		}
 		System.out.println("metodka");
 	}
 	public static void main(String[] args) {
